c8d8048c37829c0f6947354db3d3889964590952
ishimweemmanuel <ishimweemmanuel033@gmail.com>
2023-10-25 08:15:00 +0200

    this is project

---
+++ 1/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+     <div className="text-black flex justify-center text-3xl">this is the Home page</div>
+    </>
+  )
+}
+
+export default App
